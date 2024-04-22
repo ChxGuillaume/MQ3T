b7f367a492fa8321d7d65e8a5c9cc1ab6a4a933f
Guillaume Chx <43764147+ChxGuillaume@users.noreply.github.com>
2024-04-22 11:32:29 +0200

    See change logs on new update available
    Signed-off-by: Guillaume Chx <43764147+ChxGuillaume@users.noreply.github.com>

--- src/renderer/src/components/UpdateDownloadModal.vue
+++ src/renderer/src/components/UpdateDownloadModal.vue
@@ -8,6 +8,7 @@ const props = defineProps<{
 
 const emit = defineEmits<{
   'update:opened': [boolean]
+  seeChangeLogs: []
   download: []
 }>()
 
@@ -31,9 +32,27 @@ const handleUpdateDownload = () => {
         <h1 class="tw-text-xl">Update Available</h1>
         <p class="tw-mt-2 color-details">Version {{ props.version }} is available for download.</p>
       </q-card-section>
-      <q-card-actions align="right">
-        <q-btn label="Later" flat @click="dialogOpened = false" />
-        <q-btn label="Download" color="primary" @click="handleUpdateDownload" />
+      <q-card-actions align="between">
+        <q-btn
+          class="tw-text-secondary tw-justify-self-start"
+          flat
+          round
+          @click="emit('seeChangeLogs')"
+        >
+          <q-tooltip
+            class="tw-bg-secondary tw-text-black"
+            anchor="center right"
+            self="center left"
+            :offset="[5, 0]"
+          >
+            Change Logs
+          </q-tooltip>
+          <q-icon size="xs" name="fa-solid fa-note-sticky" />
+        </q-btn>
+        <div class="tw-flex tw-gap-2">
+          <q-btn label="Later" flat @click="dialogOpened = false" />
+          <q-btn label="Download" color="primary" @click="handleUpdateDownload" />
+        </div>
       </q-card-actions>
     </q-card>
   </q-dialog>
