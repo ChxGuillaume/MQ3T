097b66c0cd35fb9894ecc9cc3015b50fb0486fc8
Guillaume Chx <43764147+ChxGuillaume@users.noreply.github.com>
2024-05-07 00:26:54 +0200

    Message extra infos in history + Freeze when publishing message
    Added:
    Message extra infos in history (QoS/retained)
    
    Fix:
    Freeze inputs on Message Publish
    
    Signed-off-by: Guillaume Chx <43764147+ChxGuillaume@users.noreply.github.com>

--- src/renderer/src/tabs/TabTopics.vue
+++ src/renderer/src/tabs/TabTopics.vue
@@ -66,7 +66,9 @@ const topicTab = computed({
 
     return 'values'
   },
-  set: (value) => (topicTabRecord.value[mqttTopicsStore.selectedTopic] = value)
+  set: (value) => {
+    topicTabRecord.value[mqttTopicsStore.selectedTopic] = value
+  }
 })
 
 const handleTopicClick = (clientKey: string, topic: string) => {
@@ -107,33 +109,36 @@ const handleMessagePublished = (topic: string) => {
 }
 
 const allTopics = computed(() => {
-  return mqttTopicsStore.getAllTopicList(mqttTopicsStore.selectedConnection).sort((a, b) => {
-    const aParts = a.split('/')
-    const bParts = b.split('/')
+  return mqttTopicsStore
+    .getAllTopicList(mqttTopicsStore.selectedConnection)
+    .slice()
+    .sort((a, b) => {
+      const aParts = a.split('/')
+      const bParts = b.split('/')
 
-    const length = Math.max(aParts.length, bParts.length)
+      const length = Math.max(aParts.length, bParts.length)
 
-    for (let i = 0; i < length; i++) {
-      if (aParts[i] === undefined && bParts[i] === undefined) return 0
-      else if (aParts[i] === undefined) return -1
-      else if (bParts[i] === undefined) return 1
+      for (let i = 0; i < length; i++) {
+        if (aParts[i] === undefined && bParts[i] === undefined) return 0
+        else if (aParts[i] === undefined) return -1
+        else if (bParts[i] === undefined) return 1
 
-      const aPart = aParts[i] || ''
-      const bPart = bParts[i] || ''
+        const aPart = aParts[i] || ''
+        const bPart = bParts[i] || ''
 
-      if (aPart === bPart) continue
+        if (aPart === bPart) continue
 
-      const aIsNumber = !isNaN(Number(aPart))
-      const bIsNumber = !isNaN(Number(bPart))
+        const aIsNumber = !isNaN(Number(aPart))
+        const bIsNumber = !isNaN(Number(bPart))
 
-      if (aIsNumber && bIsNumber) return Number(aPart) - Number(bPart)
-      else if (aIsNumber) return -1
-      else if (bIsNumber) return 1
-      else return aPart.localeCompare(bPart)
-    }
+        if (aIsNumber && bIsNumber) return Number(aPart) - Number(bPart)
+        else if (aIsNumber) return -1
+        else if (bIsNumber) return 1
+        else return aPart.localeCompare(bPart)
+      }
 
-    return 0
-  })
+      return 0
+    })
 })
 
 const topicToSelect = (index: number, direction: 'up' | 'down'): string | null => {
