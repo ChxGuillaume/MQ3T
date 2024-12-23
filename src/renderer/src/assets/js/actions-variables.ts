1092d30d006c7a1489d3f5662101700b18a53292
Guillaume Chx <43764147+ChxGuillaume@users.noreply.github.com>
2024-12-23 02:49:36 +0100

    refactor: renamed variables in actions-variables.ts

--- src/renderer/src/assets/js/actions-variables.ts
+++ src/renderer/src/assets/js/actions-variables.ts
@@ -1,37 +1,35 @@
 type ActionsVariablesTypes = 'string' | 'number' | 'boolean' | 'uuidV4'
-type ActionsVariables = Record<
-  ActionsVariablesTypes,
-  {
-    type: ActionsVariablesTypes
-    regex: RegExp
-    description: string
-    isVariable?: boolean
-  }
->
 
-export const actionsVariables: ActionsVariables = {
+type ActionsVariables = {
+  type: ActionsVariablesTypes
+  regex: RegExp
+  description: string
+  isGenerated?: boolean
+}
+
+type ActionsVariablesRecord = Record<ActionsVariablesTypes, ActionsVariables>
+
+export const actionsVariables: ActionsVariablesRecord = {
   string: {
     type: 'string',
     regex: /"?\$s\$([a-zA-Z0-9_ -]+)\$"?/g,
-    description: 'This will prompt you to enter a String when executing the Action',
-    isVariable: true
+    description: 'This will prompt you to enter a String when executing the Action'
   },
   number: {
     type: 'number',
     regex: /"?\$n\$([a-zA-Z0-9_ -]+)\$"?/g,
-    description: 'This will prompt you to enter a Number when executing the Action',
-    isVariable: true
+    description: 'This will prompt you to enter a Number when executing the Action'
   },
   boolean: {
     type: 'boolean',
     regex: /"?\$b\$([a-zA-Z0-9_ -]+)\$"?/g,
-    description: 'This will prompt you to enter a Boolean when executing the Action',
-    isVariable: true
+    description: 'This will prompt you to enter a Boolean when executing the Action'
   },
   uuidV4: {
     type: 'uuidV4',
     regex: /"?\$uuid\$v4\$"?/g,
-    description: 'This will generate a UUID v4 when executing the Action'
+    description: 'This will generate a UUID v4 when executing the Action',
+    isGenerated: true
   }
 }
 
@@ -39,7 +37,7 @@ export const actionsVariablesArray = Object.values(actionsVariables)
 
 export const getPayloadVariablesGrouped = (payload: string) => {
   return actionsVariablesArray
-    .filter((item) => item.isVariable)
+    .filter((item) => !item.isGenerated)
     .map((item) => {
       return {
         type: item.type,
